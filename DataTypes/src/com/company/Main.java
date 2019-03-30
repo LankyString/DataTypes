cf4178d5fdd631d81024a3e8d93e32a3715f3397
Andrew <39748224+LankyString@users.noreply.github.com>
2019-03-30 09:45:11 +0000

    Add files via upload

---
+++ DataTypes/src/com/company/Main.java
@@ -0,0 +1,30 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // byte
+        // short
+        // int
+        // long
+        // float
+        // double
+        // char
+        // boolean
+        // String
+
+        String myString = "This is my string!";
+        System.out.println(myString);
+        myString = myString + ", and this is more.";
+        System.out.println(myString);
+        myString = myString + " \u00A9 2015";
+        System.out.println(myString);
+
+
+
+
+
+
+    }
+}
